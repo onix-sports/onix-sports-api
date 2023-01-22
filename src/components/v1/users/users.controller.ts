814f4fc0eb5bcec92c6f8916cf5158b8ddfa405f
Vladyslav Koropets <vladyslav.koropets@onix-systems.com>
2023-01-22 21:05:11 +0100

    feat(users-me): add /v1/users/me route

--- src/components/v1/users/users.controller.ts
+++ src/components/v1/users/users.controller.ts
@@ -8,18 +8,35 @@ import {
 } from '@nestjs/swagger';
 import validationPipe from '@pipes/validation.pipe';
 import { SchemaObject } from '@nestjs/swagger/dist/interfaces/open-api-spec.interface';
+import RequestUser from '@decorators/request-user.decorator';
+import JwtPayloadDto from '@components/v1/auth/dto/jwt-payload.dto';
+import Authorized from '@decorators/authorized.decorator';
 import { GetUserDto } from './dto/get-user.dto';
 import { GetUsersDto } from './dto/get-users.dto';
 import { UsersService } from './users.service';
 import { User } from './schemas/user.schema';
 
 @ApiTags('Users')
-@ApiExtraModels(User)
+@ApiExtraModels(User, JwtPayloadDto)
 @Controller('users')
 export class UsersController {
     constructor(
     private readonly usersService: UsersService,
     ) {}
+
+    @ApiResponse({
+        properties: {
+            type: 'object',
+            $ref: getSchemaPath(JwtPayloadDto),
+        } as SchemaObject,
+        description: 'Returns authenticated user',
+    })
+    @HttpCode(HttpStatus.OK)
+    @Get('/me')
+    @Authorized()
+    public getMe(@RequestUser() user: JwtPayloadDto) {
+        return user;
+    }
 
     @ApiResponse({
         properties: {
